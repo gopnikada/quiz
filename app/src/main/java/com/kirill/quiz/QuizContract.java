2a648b1bf81db569fc5d8d88306c19894d2b6a36
gopnikada <klarsky.kirill6@gmail.com>
2021-01-09 18:19:36 +0300

    nodelling database

---
+++ app/src/main/java/com/kirill/quiz/QuizContract.java
@@ -0,0 +1,14 @@
+package com.kirill.quiz;
+
+import android.provider.BaseColumns;
+
+public final class QuizContract implements BaseColumns {
+    public static class QuestionsTable{ //database table model
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTION = "question";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
+        public static final String COLUMN_ANSWER_NR = "answer_nr";
+    }
+}
