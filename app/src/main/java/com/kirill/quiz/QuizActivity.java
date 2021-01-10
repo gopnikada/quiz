47094a53102851ff7bb7c73ab190ef0448e102a6
gopnikada <klarsky.kirill6@gmail.com>
2021-01-10 12:33:56 +0300

    sharedPrefs -> highscore

--- app/src/main/java/com/kirill/quiz/QuizActivity.java
+++ app/src/main/java/com/kirill/quiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.kirill.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+    public static final String EXTRA_SCORE = "extraScore";
     private TextView textViewQuestion;
     private TextView textViewScore;
     private TextView textViewCountDown;
@@ -133,6 +135,10 @@ public class QuizActivity extends AppCompatActivity {
         }
     }
     private void finishQuiz() {
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
         finish();
     }
 }
--- app/src/main/java/com/kirill/quiz/StartingScreenActivity.java
+++ app/src/main/java/com/kirill/quiz/StartingScreenActivity.java
@@ -1,18 +1,31 @@
 package com.kirill.quiz;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StartingScreenActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QUIZ = 1;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighscore";
+
+    TextView textViewHighscore;
+
+    private int highscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        textViewHighscore = findViewById(R.id.text_view_highscore);
+        loadHighscore();//loading highscore
 
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz); //init start button
 
@@ -27,6 +40,35 @@ public class StartingScreenActivity extends AppCompatActivity {
 
     private void startQuiz() { //starting quiz activity
         Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);//starting activity and waiting for a result
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_QUIZ){
+            if(resultCode == RESULT_OK){
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if(score > highscore){
+                    updateHighScore(score);
+                }
+            }
+        }
+    }
+    private void loadHighscore(){
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighscore.setText("HighScore: " + highscore);
+    }
+
+    private void updateHighScore(int highscoreNew) {
+        highscore = highscoreNew;
+        textViewHighscore.setText("HighScore: " + highscore);
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
     }
 }
